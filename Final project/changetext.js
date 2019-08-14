7dc9e87dd5b1fe25cc5a3963294c7fda0560b8d1
j-a-s-m-i-n-e <53236301+j-a-s-m-i-n-e@users.noreply.github.com>
2019-08-14 12:50:51 -0400

    Add files via upload

---
+++ Final project/changetext.js
@@ -0,0 +1,40 @@
+
+function changetext() {
+
+var elements = document.getElementsByClassName('changetext');
+
+for (var i = 0; i < elements.length; i++) {
+ var element = elements[i];
+ element.style.fontSize = "35px";
+}
+}
+
+function changetext1() {
+
+var elements = document.getElementsByClassName('changetext');
+
+for (var i = 0; i < elements.length; i++) {
+ var element = elements[i];
+ element.style.fontSize = "30px";
+}
+}
+
+function changetext2() {
+
+var elements = document.getElementsByClassName('changetext');
+
+for (var i = 0; i < elements.length; i++) {
+ var element = elements[i];
+ element.style.fontSize = "25px";
+}
+}
+
+function normaltext() {
+
+var elements = document.getElementsByClassName('changetext');
+
+for (var i = 0; i < elements.length; i++) {
+ var element = elements[i];
+ element.style.fontSize = "16px";
+}
+}
